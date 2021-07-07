e94c5077a1a912fe3e9388d87baad48be0b3d9df
jawad <jawadhalbymakary@gmail.com>
2021-07-07 13:14:50 +0300

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyC5J_WHi5W_XZDr-b0n_PYK7h9cLGR2ZFo",
+    authDomain: "teslaclone-77a55.firebaseapp.com",
+    projectId: "teslaclone-77a55",
+    storageBucket: "teslaclone-77a55.appspot.com",
+    messagingSenderId: "40280074392",
+    appId: "1:40280074392:web:8077303729de4beafdc8ed",
+    measurementId: "G-3WWVDRH6EX"
+  };
+  const firebaseApp=firebase.initializeApp(firebaseConfig)
